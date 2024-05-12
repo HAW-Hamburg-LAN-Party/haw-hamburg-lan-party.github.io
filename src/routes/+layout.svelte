556f5ecbfbbb633a24898164afa84f34de46dd41
Julian Hoffmann <julian@jhgo.de>
2024-05-13 00:10:17 +0200

    adds tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Footer from '$lib/components/Footer.svelte';
-import Header from '$lib/components/Header.svelte';
-	import '../global.scss';
+	import Header from '$lib/components/Header.svelte';
+	import '../app.scss';
 </script>
 
 <Header />
