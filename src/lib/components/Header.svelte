b2e60b7902fc68db261a8fc25c24227f601fef61
Julian Hoffmann <julian@jhgo.de>
2024-06-03 14:13:48 +0200

    adds close icon to mobile navigation

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -27,16 +27,27 @@
 				aria-controls="menu"
 				on:click={() => (showMobileMenu = !showMobileMenu)}
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					height="24px"
-					viewBox="0 0 24 24"
-					width="24px"
-					class="fill-primary"
-					><path d="M0 0h24v24H0z" fill="none" /><path
-						d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
-					/></svg
-				>
+				{#if showMobileMenu}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						class="fill-primary w-full h-full"
+					>
+						<title>close</title>
+						<path
+							d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+						/>
+					</svg>
+				{:else}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="fill-primary w-full h-full"
+						viewBox="0 0 24 24"
+					>
+						<title>menu</title>
+						<path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+					</svg>
+				{/if}
 			</button>
 			<!-- navigation desktop links -->
 			<nav
@@ -64,8 +75,7 @@
                     sm:hidden
                     items-center
                     justify-between
-                    w-full
-                    mt-[1px]"
+                    w-full"
 				>
 					<Navbar />
 				</nav>
