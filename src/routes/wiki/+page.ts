0aa027438011a1e66fc5455b05f497321a754403
Julian Hoffmann <julian@jhgo.de>
2024-06-04 11:44:01 +0200

    removes console log

--- src/routes/wiki/+page.ts
+++ src/routes/wiki/+page.ts
@@ -1,4 +1,4 @@
-import { error } from '@sveltejs/kit'
+import { error } from '@sveltejs/kit';
 import GithubSlugger from 'github-slugger';
 
 interface IRemarkHeading {
@@ -7,7 +7,7 @@ interface IRemarkHeading {
 }
 
 export interface IHeading {
-	title: string,
+	title: string;
 	href: string;
 	level: number;
 	subtitles?: IHeading[];
@@ -16,61 +16,55 @@ export interface IHeading {
 const WIKI_MAX_NAV_DEPTH = 3;
 
 function pushHeading(headingList: IHeading[], currentHeader: IHeading) {
-	const size = headingList.length
+	const size = headingList.length;
 
 	if (size <= 0) {
-		headingList.push(currentHeader)
+		headingList.push(currentHeader);
 		return;
 	}
-	const previous = headingList[headingList.length - 1]
+	const previous = headingList[headingList.length - 1];
 
 	if (currentHeader.level <= previous.level) {
-		headingList.push(currentHeader)
+		headingList.push(currentHeader);
 		return;
 	}
 	if (previous.subtitles === undefined) {
-		previous.subtitles = []
+		previous.subtitles = [];
 	}
-	pushHeading(previous.subtitles, currentHeader)
+	pushHeading(previous.subtitles, currentHeader);
 }
 
 export async function load() {
 	try {
 		//@ts-expect-error only a ide problem
-		const site = await import(`../../content/wiki.md`)
+		const site = await import(`../../content/wiki.md`);
 
 		const headings: IRemarkHeading[] = site.metadata.headings;
 
-		const initalValue: IHeading[] = []
+		const initalValue: IHeading[] = [];
 		const slugger = new GithubSlugger();
 
 		const headers = headings.reduce((resaltArray, curr) => {
 			if (curr.level > WIKI_MAX_NAV_DEPTH) {
-				return resaltArray
+				return resaltArray;
 			}
 			const currentHeading: IHeading = {
 				title: curr.title,
 				href: `#${slugger.slug(curr.title)}`,
-				level: curr.level,
-			}
+				level: curr.level
+			};
 
+			pushHeading(resaltArray, currentHeading);
 
-			pushHeading(resaltArray, currentHeading)
-
-
-			return resaltArray
-		}, initalValue)
-
-		console.log(headers);
-		
-		
+			return resaltArray;
+		}, initalValue);
 
 		return {
 			content: site.default,
 			meta: site.metadata,
-			headers: headers,
-		}
+			headers: headers
+		};
 	} catch (e) {
-		error(404, `Could not find Wiki`)
+		error(404, `Could not find Wiki`);
 	}
 }
