a9dd8b6bafb8ebf82a6df7b8a854464101fba7bd
Julian Hoffmann <julian@jhgo.de>
2024-06-05 21:56:12 +0200

    fixes sitemap double slashes

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
-export const siteURL = 'https://haw-lan.de/';
+export const siteURL = 'https://haw-lan.de';
 export const staticRoutes = ['/'];
 export const blacklistMarkdownPage = ['home', 'navbar'];
 export const wikiMaxNavDepth = 3;
