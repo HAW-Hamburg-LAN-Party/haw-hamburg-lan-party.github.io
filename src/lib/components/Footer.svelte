e7ae605a32504767d8fc378e526d825a21b4239b
DomCie <dev@domcie.net>
2025-04-17 21:04:08 +0200

    [components] Update copyright

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="mt-4 py-2 border-t-border border-t-[1px] bg-page">
     <div class="center-content">
-        <p class="text-center text-border">Copyright © 2024 HAW Hamburg LAN-Party | <a href="/datenschutzerklaerung">Datenschutzerklärung</a> | <a href="/impressum">Impressum</a></p>
+        <p class="text-center text-border">Copyright © 2025 HAW Hamburg LAN-Party | <a href="/datenschutzerklaerung">Datenschutzerklärung</a> | <a href="/impressum">Impressum</a></p>
     </div>
 </footer>
